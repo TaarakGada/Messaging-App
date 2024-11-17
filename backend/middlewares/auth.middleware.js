ac73741a312ed43cea80ad9b0611954c850c5924
Taarak Gada <taarakgada4151@gmail.com>
2024-11-17 21:51:52 +0530

    update cookie sameSite option to 'Lax' in authentication controller and refactor error messages in auth middleware

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,29 +1,27 @@
-import { User } from "../models/user.model.js";
-import { ApiError } from "../utils/APIError.js";
-import { asyncHandler } from "../utils/asynchandler.js";
-import jwt from "jsonwebtoken";
+import { User } from '../models/user.model.js';
+import { ApiError } from '../utils/APIError.js';
+import { asyncHandler } from '../utils/asynchandler.js';
+import jwt from 'jsonwebtoken';
 
 export const verifyJWT = asyncHandler(async (req, _, next) => {
     try {
-        const token =
-            req.cookies?.accessToken ||
-            req.header("Authorization")?.replace("Bearer ", "");
+        const token = req.cookies?.accessToken;
         if (!token) {
-            throw new ApiError(401, "Unauthorized request");
+            throw new ApiError(401, 'Unauthorized request');
         }
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
         const user = await User.findById(decodedToken?._id).select(
-            " -password -refreshToken"
+            ' -password -refreshToken'
         );
 
         if (!user) {
-            throw new ApiError(401, "Invalid Access token.");
+            throw new ApiError(401, 'Invalid Access token.');
         }
 
         req.user = user;
         next();
     } catch (error) {
-        throw new ApiError(401, error?.message || "Invalid access token.");
+        throw new ApiError(401, error?.message || 'Invalid access token.');
     }
 });
